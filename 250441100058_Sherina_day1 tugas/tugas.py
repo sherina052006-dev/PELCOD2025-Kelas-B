f269f400f7c6b906a488a02a1c98bc028d2ea9b7
sherina052006-dev <sherina.052006@gmail.com>
2025-09-19 13:49:53 +0700

    Pengumpulan Tugas Pelcod Sherina

---
+++ 250441100058_Sherina_day1 tugas/tugas.py
@@ -0,0 +1,11 @@
+tugas = float(input ("Masukkan Nilai Tugas: "))
+uts = float(input ("Masukkan Nilai uts: "))
+uas = float(input ("Masukkan Nilai uas: ")) 
+
+nilai_tugas = 0.30
+nilai_uts = 0.30
+nilai_uas =0.40
+
+nilai_akhir = (tugas * nilai_tugas) + (uts * nilai_uts) + (uas * nilai_uas )
+
+print("Nilai Akhir Andi:", nilai_akhir)
---
+++ 250441100058_Sherina_day1/pelcod.py
@@ -0,0 +1,29 @@
+# nama = "sherina"
+# nim = 250441100058
+# semester = 1
+
+# print ("=====BIODATA MAHASISWA=====")
+# print ("Hallo, nama saya :"+ nama + "\n")
+# print (f"NIM saya : {nim}\n")
+# print ("saya berkuliah di semester : {}". format (semester))
+
+# a = 10
+# b = 5
+# c = a + b # Penjumlahan 
+# d = a - b # Pengurangan 
+# e = a * b # Perkalian 
+# f = a / b # Pembagian 
+# g = a % b # Sisa bagi 
+# print("Hasil Penjumlahan:", c)
+# print("Hasil Pengurangan:", d)
+# print("Hasil Perkalian:", e)
+# print("Hasil Pembagian:", f)
+# print("Hasil Sisa bagi:", g)
+
+nama = input("Masukkan nama Anda: ")
+umur = input("Masukkan umur Anda: ")
+buah = input("Masukkan buah favorit Anda: ")
+
+print("Nama saya adalah " + nama)
+print("Umur saya adalah " + umur + " tahun")
+print("Buah favorit saya adalah " + buah)
